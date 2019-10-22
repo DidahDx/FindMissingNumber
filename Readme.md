02bf20ba48a0bdf18b3e4fa7d82a6259f7d3c122
Daniel Didah <didahdaniel@gmail.com>
2019-10-22 17:38:52 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+## Finding the Missing Number in the Array
+### Question
+Write a function.
+
+Given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 4, 1, 5, 2, 7], the function should return 6.
+
+Given A = [1, 2, 3, 4, 5], the function should return 6.
+
+Given A = [−5, −6], the function should return 1.
---
+++ main.py
@@ -0,0 +1,21 @@
+# checks for the first missing number in array
+def check_array(num):
+    # check if array only contains numbers less than zero
+    if max(num) > 0 or min(num) > 0:
+        # loop through from 1 to max value in array
+        for x in range(1, max(num) + 1):
+            # check if the number is in the array
+            if x in num:
+                # check if loop reaches the max value
+                if x == max(num):
+                    return x + 1
+                continue
+            else:
+                return x
+    else:
+        return 1
+
+
+numbers = [-2, 5, 12, 7, 1, 3, -4, 2, 6, 8]
+numbers=[1, 3, 4, 1, 5, 2,7]
+print(check_array(numbers))
